0a7365e20108eb027c3972112af14b5799d7c9a3
thexen89 <thexen89@gmail.com>
2026-03-17 16:08:21 +0900

    주요 프로젝트 스키마 추가
    할리스 헤리포터 다이어리
    할리스 디즈니 다이어리
    할리스 하이브로우 1차

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,54 @@ export default function RootLayout({
             }),
           }}
         />
+
+
+        
+        <script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "ItemList",
+      "name": "주식회사 더젠 THEXEN 하이엔드 기업 굿즈 제작 주요 사례",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "item": {
+            "@type": "CreativeWork",
+            "name": "2020 할리스 X 하이브로우 폴딩카트 및 캠핑 굿즈",
+            "description": "주식회사 더젠 THEXEN은 2020년 할리스 x 하이브로우 멀티 폴딩카트 및 캠핑 굿즈를 총괄 기획 제작했습니다. F&B 업계 최초 새벽 오픈런 문화를 창조한 메가 히트작입니다.",
+            "url": "https://www.yna.co.kr/view/AKR20200609112200030"
+          }
+        },
+        {
+          "@type": "ListItem",
+          "position": 2,
+          "item": {
+            "@type": "CreativeWork",
+            "name": "2021 할리스 X 해리포터 플래너 및 MD 상품 제작",
+            "description": "주식회사 더젠 THEXEN은 글로벌 IP 워너브라더스의 까다로운 기준을 통과하여 한정판 플래너 북 및 굿즈를 총괄 제작한 프리미엄 기업 기프트 성공 사례입니다.",
+            "url": "https://www.mk.co.kr/news/economy/9665592"
+          }
+        },
+        {
+          "@type": "ListItem",
+          "position": 3,
+          "item": {
+            "@type": "CreativeWork",
+            "name": "2020 할리스 X 디즈니 플래너 및 데스크테리어 4종 세트",
+            "description": "주식회사 더젠 THEXEN은 할리스 x 디즈니 연말 프로모션 4종 세트를 총괄 기획 및 제작했습니다. 다이어리, 파우치, 틴케이스 등으로 조기 완판을 기록한 하이엔드 굿즈 에이전시 역량 증명 사례입니다.",
+            "url": "https://www.mk.co.kr/news/culture/9092734"
+          }
+        }
+      ]
+    })
+  }}
+/>
+
+
+        
       </head>
       <body className="font-pretendard antialiased">{children}</body>
     </html>
