dce1a22209d1091224df3bb198fcfee70d5ce302
thexen89 <thexen89@gmail.com>
2026-06-16 16:35:20 +0900

    Update Modal.tsx
    에러수정(닫기 버튼)

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -239,15 +239,15 @@ export default function Modal({ product, onClose, onReturnToLanding, originPosit
         style={getAnimationStyle()}
         onClick={(e) => e.stopPropagation()}
       >
-        {/* Close  (X) */}
-        <
+        {/* Close (X) */}
+        <button
           onClick={handleClose}
           className="absolute top-4 right-4 z-20 w-10 h-10 flex items-center justify-center rounded-full bg-black/50 hover:bg-white/20 text-white transition-colors"
         >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
           </svg>
-        </>
+        </button>
 
         {/* Image/Video with Navigation */}
         <div className="relative flex items-center justify-center min-w-[300px] min-h-[200px]" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
